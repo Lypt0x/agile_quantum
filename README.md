9e109702771d085195efbe4128c5491aa02ff140
Lypt0x <76274659+Lypt0x@users.noreply.github.com>
2021-02-13 00:12:07 +0100

    Compute quantum

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # agile_quantum
-C++17 Quantum Compute
+C++20 Quantum Compute
+
+## Building
+Clone the repository with its submodules <code>git clone --recursive-submodules https://github.com/Lypt0x/agile_quantum.git</code>
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,14 @@
 // Created by lypt0 on 2/12/2021.
 //
 
-int main(void) {
+#include <iostream>
+#include <qpp.h>
 
+int main() {
+
+    qpp::ket psi = qpp::st.z0;
+    qpp::cmat u = qpp::gt.X;
+    qpp::ket result = u * psi;
+
+    std::cout << "|0> bit-flip gate X \n" << qpp::disp(result);
 }
